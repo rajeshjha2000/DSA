2713462263a637197d8ecfaaf2459f0e9d650433
rajeshjha2000 <rajeshkumarjha153@gmail.com>
2024-05-14 22:27:17 +0530

    sixth commit

---
+++ arrays/CommonElementIn3SortedArray(gfg).cpp
@@ -0,0 +1,34 @@
+
+// class Solution
+// {
+//     public:    
+//        vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+//         {
+//             vector<int> ans;
+//             set<int> st;
+//             int i=0; int j=0; int k=0;
+            
+//             while(i<n1 && j<n2 && k<n3){
+                
+//                 if(A[i]==B[j] && B[j]==C[k] && C[k]==A[i]){
+//                 st.insert(A[i]);
+//                 i++, j++, k++;}
+                
+//                 else if(A[i]<B[j]){
+//                     i++;}
+                    
+//                     else if(B[j]<C[k]){
+//                         j++;}
+                        
+//                         else {k++;}
+//                     }
+                    
+//                     for (auto i : st)
+//                     { ans.push_back(i);}
+                
+               
+//                return ans; 
+// } 
+//         }
+
+// //{ Driver Code Starts.
