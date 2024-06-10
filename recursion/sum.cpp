46fcdd81d1b4eba36b73d863adb2170bff581233
rajeshjha2000 <rajeshkumarjha153@gmail.com>
2024-06-10 13:49:48 +0530

    first commit

---
+++ recursion/sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int sum(int n){
+    // base case
+    if(n==1){
+        return 1;
+    }
+    // recursive relation
+    int ans = n + sum(n-1);
+    return ans;
+}
+
+int main(){
+   cout<<sum(10)<<endl;
+}
